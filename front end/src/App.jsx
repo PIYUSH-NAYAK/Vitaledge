9a39eb81b5c67a2025039d7997067dc08f79de76
PIYUSH-IIITL <piyushnayak90020@gmail.com>
2024-12-16 18:55:25 +0530

    added navabr

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import Navbar2 from "../mycomp/nav";
 
-function App() {
-  const [count, setCount] = useState(0)
+import NavBar from "../mycomp/nav";
+// import Nav2 from "../mycomp/nav";
+// import Navbar from "../mycomp/nav";
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-screen h-screen bg-red-200 ">
+        <div>
+          <NavBar />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
 
-export default App
+      {/* <div><Navbar2/> </div> */}
+    </>
+  );
+}

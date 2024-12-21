e35bef937eaa39e31d5166c1e016d05524f45602
Swyamk <swyamkumar2428@gmail.com>
2024-12-21 13:18:43 +0530

    addition of contact us page

--- front end/src/App.jsx
+++ front end/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './mycomp/sec1';
 import About from './mycomp/Aboutus';
 import Nav2 from './mycomp/Nav';
 // import ContactPage from './mycomp/Contactus';
+import ContactPage from './mycomp/Contactus';
 
 function App() {
   return (
@@ -15,7 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/AboutUs" element={<About />} />
-        {/* <Route path="/ContactUs" element={<ContactPage />} /> */}
+        <Route path="/ContactUs" element={<ContactPage />} />
       </Routes>
     </Router>
   );

ab2519f958bf62e7c0ba240186c6000cadb2fabe
Swyamk <swyamkumar2428@gmail.com>
2025-02-06 19:43:25 +0530

    changing the port values

--- front end/vite.config.js
+++ front end/vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 6, // Change this to your preferred port
+    port: 5000, // Change this to your preferred port
   },
 })

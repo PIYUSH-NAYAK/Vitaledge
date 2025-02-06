6cd308e53a6902b7bb648e1cdc4cf42a65dd9f91
Swyamk <swyamkumar2428@gmail.com>
2025-02-06 16:30:18 +0530

    updated navbar

--- front end/src/components/Pricing.jsx
+++ front end/src/components/Pricing.jsx
@@ -28,8 +28,8 @@ const Pricing = () => {
         </div>
 
         <Heading
-          tag="Get started with Brainwave"
-          title="Pay once, use forever"
+          tag="Get started with Vitaledge"
+          title="Connect once, stay forever"
         />
 
         {/* <div className="relative">
